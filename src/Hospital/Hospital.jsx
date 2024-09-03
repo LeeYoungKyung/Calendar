3482acdfed310dea1e455a57898488fb6349a5d0
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-09-04 00:06:31 +0900

    [add] pages

---
+++ src/Hospital/Hospital.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+//카카오 api를 사용해서 주위에 있는 병원들을 보여주고 옆에는 리스트를 보여주기
+const Hospital = () => {
+  return <div>Hospital</div>;
+};
+
+export default Hospital;
---
+++ src/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+//웹의 목적 사진으로 애니메이션 좀 넣고
+const Main = () => {
+  return (
+    <div>
+      <div className='bg-slate-100 h-lvh'></div>
+    </div>
+  );
+};
+
+export default Main;
