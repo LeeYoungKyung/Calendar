be8354529f721306494006f661b32e427488fc33
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-09-05 04:08:27 +0900

    [design] tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,3 +8,21 @@ module.exports = {
   },
   plugins: [],
 };
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        rotate: 'rotate 1.5s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
