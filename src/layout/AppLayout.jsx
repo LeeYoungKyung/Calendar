0f89549bc594fb1eca9d3325641731e12d74ff0b
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-09-08 15:32:10 +0900

    [fix] layout 중요도에 맞게 다시 배치하기

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -10,13 +10,15 @@ const AppLayout = () => {
     <div>
       <Navbar expand='lg' className='bg-body-tertiary'>
         <Container className='w-[80%]'>
-          <Navbar.Brand href='/'>로고</Navbar.Brand>
+          <Navbar.Brand href='/'>
+            <img src='/logo.png' alt='Logo' className='h-10 w-auto' />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href='/diary'>diary</Nav.Link>
-              <Nav.Link href='/hospital'>hospital</Nav.Link>
-              <Nav.Link href='/medicine'>medicine</Nav.Link>
+              <Nav.Link href='/diary'>일정관리</Nav.Link>{' '}
+              <Nav.Link href='/medicine'>약찾기</Nav.Link>
+              <Nav.Link href='/hospital'>병원/약국</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
